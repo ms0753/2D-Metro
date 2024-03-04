7e1353a9a9db7d292fad36ba7bd8bb0b6c6f5483
ms0753 <129492560+ms0753@users.noreply.github.com>
2024-03-04 20:16:05 +0900

    [Add] Player 점프 구현

--- 2D_Metro/Assets/Scripts/Player.cs
+++ 2D_Metro/Assets/Scripts/Player.cs
@@ -19,6 +19,8 @@ public class Player : MonoBehaviour
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
 
     #endregion
 
@@ -28,6 +30,8 @@ public class Player : MonoBehaviour
         stateMachine = new PlayerStateMachine();
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start()
--- 2D_Metro/Assets/Scripts/PlayerAirState.cs
+++ 2D_Metro/Assets/Scripts/PlayerAirState.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerIdleState : PlayerGroundedState
+public class PlayerAirState : PlayerState
 {
-    public PlayerIdleState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    public PlayerAirState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
 
@@ -21,7 +21,8 @@ public class PlayerIdleState : PlayerGroundedState
     public override void Update()
     {
         base.Update();
-        if(xInput != 0)
-            stateMachine.ChangeState(player.moveState);
+
+        if (rb.velocity.y == 0)
+            stateMachine.ChangeState(player.idleState);
     }
 }
